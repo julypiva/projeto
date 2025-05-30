58868cf59f1acf1cdf445d76bc7764dfb51e1600
julypiva <julyapiva11@gmail.com>
2025-05-30 01:03:24 -0300

    Update app.py
    Signed-off-by: julypiva <julyapiva11@gmail.com>

--- Projeto 2/app.py
+++ Projeto 2/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request
-from selenium_script import iniciar_monitoramento
-from analise_sentimento import processar_analise_sentimento
+from flask import Flask, render_template, request # Importa a classe Flask e as funções para renderizar templates HTML e lidar com requisições HTTP
+from selenium_script import iniciar_monitoramento # Importa a função que inicia o monitoramento via Selenium (automatização de navegador)
+from analise_sentimento import processar_analise_sentimento # Importa a função que realiza a análise de sentimento em textos
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'], endpoint='index')
 def index():
